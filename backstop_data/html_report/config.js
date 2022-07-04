1eaac7bedc8a1f16cfdc9ca4c07955424b30c2de
kartashevadidi <80789540+kartashevadidi@users.noreply.github.com>
2022-07-04 15:13:31 +0300

    3

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,65 +3,18 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\IDF_Subscribe_EN_0_document_0_phone-320.png",
-        "test": "..\\bitmaps_test\\20220630-203549\\IDF_Subscribe_EN_0_document_0_phone-320.png",
-        "selector": "document",
-        "fileName": "IDF_Subscribe_EN_0_document_0_phone-320.png",
-        "label": "Subscribe_EN",
+        "reference": "..\\bitmaps_reference\\IDF_CARRERARopenError_ES_0__0_phone-320.png",
+        "test": "..\\bitmaps_test\\20220704-160451\\IDF_CARRERARopenError_ES_0__0_phone-320.png",
+        "selector": "",
+        "fileName": "IDF_CARRERARopenError_ES_0__0_phone-320.png",
+        "label": "CARRERARopenError_ES",
         "misMatchThreshold": 0.1,
-        "url": "http://moneyman:1005@qa-delivery-idfinance-ru-master.moneyman.ru",
-        "referenceUrl": "https://idfinance.com/about/",
+        "url": "http://moneyman:1005@qa-delivery-idfinance-ru-master.moneyman.ru/es/carreras/",
+        "referenceUrl": "https://idfinance.com/es/carreras/",
         "expect": 0,
         "viewportLabel": "phone-320",
-        "error": "Reference file not found C:\\Users\\diana.kartasheva\\IdeaProjects\\idf_backstope\\backstop_data\\bitmaps_reference\\IDF_Subscribe_EN_0_document_0_phone-320.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\IDF_Subscribe_ES_0_document_0_phone-320.png",
-        "test": "..\\bitmaps_test\\20220630-203549\\IDF_Subscribe_ES_0_document_0_phone-320.png",
-        "selector": "document",
-        "fileName": "IDF_Subscribe_ES_0_document_0_phone-320.png",
-        "label": "Subscribe_ES",
-        "misMatchThreshold": 0.1,
-        "url": "http://moneyman:1005@qa-delivery-idfinance-ru-master.moneyman.ru",
-        "referenceUrl": "https://idfinance.com/about/",
-        "expect": 0,
-        "viewportLabel": "phone-320",
-        "error": "Reference file not found C:\\Users\\diana.kartasheva\\IdeaProjects\\idf_backstope\\backstop_data\\bitmaps_reference\\IDF_Subscribe_ES_0_document_0_phone-320.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\IDF_SubscribeError_EN_0_document_0_phone-320.png",
-        "test": "..\\bitmaps_test\\20220630-203549\\IDF_SubscribeError_EN_0_document_0_phone-320.png",
-        "selector": "document",
-        "fileName": "IDF_SubscribeError_EN_0_document_0_phone-320.png",
-        "label": "SubscribeError_EN",
-        "misMatchThreshold": 0.1,
-        "url": "http://moneyman:1005@qa-delivery-idfinance-ru-master.moneyman.ru",
-        "referenceUrl": "https://idfinance.com/",
-        "expect": 0,
-        "viewportLabel": "phone-320",
-        "error": "Reference file not found C:\\Users\\diana.kartasheva\\IdeaProjects\\idf_backstope\\backstop_data\\bitmaps_reference\\IDF_SubscribeError_EN_0_document_0_phone-320.png"
-      },
-      "status": "fail"
-    },
-    {
-      "pair": {
-        "reference": "..\\bitmaps_reference\\IDF_SubscribeError_ES_0_document_0_phone-320.png",
-        "test": "..\\bitmaps_test\\20220630-203549\\IDF_SubscribeError_ES_0_document_0_phone-320.png",
-        "selector": "document",
-        "fileName": "IDF_SubscribeError_ES_0_document_0_phone-320.png",
-        "label": "SubscribeError_ES",
-        "misMatchThreshold": 0.1,
-        "url": "http://moneyman:1005@qa-delivery-idfinance-ru-master.moneyman.ru",
-        "referenceUrl": "https://idfinance.com/about/",
-        "expect": 0,
-        "viewportLabel": "phone-320",
-        "error": "Reference file not found C:\\Users\\diana.kartasheva\\IdeaProjects\\idf_backstope\\backstop_data\\bitmaps_reference\\IDF_SubscribeError_ES_0_document_0_phone-320.png"
+        "engineErrorMsg": "waiting for selector `#position_form > div.user_metrick > div:nth-child(1) > input[type=text]` failed: timeout 30000ms exceeded",
+        "error": "Reference file not found C:\\Users\\diana.kartasheva\\IdeaProjects\\idf_backstope\\backstop_data\\bitmaps_reference\\IDF_CARRERARopenError_ES_0__0_phone-320.png"
       },
       "status": "fail"
     }
